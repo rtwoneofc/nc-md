2e2616d85be43104f45b9e6d944ec1f98e0f51fc
IzumiMD <107867501+IzumiMD@users.noreply.github.com>
2022-06-27 21:35:34 +0700

    Add files via upload

--- message/msg.js
+++ message/msg.js
@@ -882,7 +882,7 @@ _5. Russia / +7 = 3K_
 \`\`\`TIDAK MENERIMA VIPUL
 SCAN QR DI ATAS PEMBAYARAN BISA MELAUI = GOPAY - OVO - DANA - SHOOPEPAY - DLL\`\`\``
 var kode = [{buttonId: `/owner`, buttonText: { displayText: "Owner" }, type: 1 }]
-				conn.sendMessage(from, { caption: teks, image: { url: `https://bit.ly/3OozGwE` }, buttons: kode, footer: 'Silakan Hubungi Owner Untuk Melanjutkan Pesananmu' }, { quoted: fgclink })
+				conn.sendMessage(from, { caption: teks, image: { url: `https://bit.ly/3OozGwE` }, buttons: kode, footer: 'Silakan Hubungi Owner Untuk Melanjutkan Pesananmu' }, { quoted: fake })
 				break
 case prefix+'runtime':
 case prefix+'tes':
