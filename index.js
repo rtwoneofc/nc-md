d9fe02def4cdb45896814c5a8ef7ad3b25447988
IzumiMD <107867501+IzumiMD@users.noreply.github.com>
2022-07-05 22:17:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,167 @@
-/*
-Base : Irfan / Chitanda
-Created By : Arasya
-Record By : Christian ID
-*/
+/*HELLO BRO TERIMA KASIH SUDAH AMBIL SCRIPT INI JANGAN LUPA IKUTI DAN SUBSCRIBE MEDIA SOSIAL OWNER DAN BOT
 
-function _0x17fd(_0x4de836,_0x4e0943){const _0x478103=_0x17ea();return _0x17fd=function(_0x3f6254,_0x37d990){_0x3f6254=_0x3f6254-0x176;let _0x548aa3=_0x478103[_0x3f6254];return _0x548aa3;},_0x17fd(_0x4de836,_0x4e0943);}(function(_0x7d19ef,_0x1d8756){const _0x3c4249=_0x7d19ef();function _0xc999a7(_0x3e5cdd,_0x5dbdeb){return _0x17fd(_0x3e5cdd- -0x3b5,_0x5dbdeb);}while(!![]){try{const _0x5c38f2=-parseInt(_0xc999a7(-0x21b,-0x1ff))/0x1*(-parseInt(_0xc999a7(-0x1d9,-0x1b4))/0x2)+-parseInt(_0xc999a7(-0x1d5,-0x1f5))/0x3*(-parseInt(_0xc999a7(-0x1e7,-0x1d7))/0x4)+-parseInt(_0xc999a7(-0x20b,-0x1fb))/0x5+parseInt(_0xc999a7(-0x1f5,-0x217))/0x6+parseInt(_0xc999a7(-0x1eb,-0x1e7))/0x7*(-parseInt(_0xc999a7(-0x225,-0x1ff))/0x8)+parseInt(_0xc999a7(-0x20f,-0x20a))/0x9*(-parseInt(_0xc999a7(-0x1d2,-0x1d0))/0xa)+-parseInt(_0xc999a7(-0x1ee,-0x1b7))/0xb*(parseInt(_0xc999a7(-0x1e8,-0x21e))/0xc);if(_0x5c38f2===_0x1d8756)break;else _0x3c4249['push'](_0x3c4249['shift']());}catch(_0x5eca06){_0x3c4249['push'](_0x3c4249['shift']());}}}(_0x17ea,0xe1897));const _0x3d51ed=(function(){let _0x5f212c=!![];return function(_0x2694da,_0x545e97){const _0x3ac1d3=_0x5f212c?function(){function _0x4a0785(_0x5c6721,_0x1e6b06){return _0x17fd(_0x1e6b06- -0x115,_0x5c6721);}if(_0x545e97){const _0x456ca5=_0x545e97[_0x4a0785(0x50,0x63)](_0x2694da,arguments);return _0x545e97=null,_0x456ca5;}}:function(){};return _0x5f212c=![],_0x3ac1d3;};}()),_0x339bd7=_0x3d51ed(this,function(){function _0x5bfcb4(_0xf66da6,_0x5365ce){return _0x17fd(_0x5365ce- -0x3d8,_0xf66da6);}return _0x339bd7['toString']()['search'](_0x5bfcb4(-0x25b,-0x225))['toString']()[_0x5bfcb4(-0x24b,-0x21c)](_0x339bd7)[_0x5bfcb4(-0x244,-0x212)](_0x5bfcb4(-0x23a,-0x225));});_0x339bd7();function _0x17ea(){const _0x13ff68=['https://youtube.com/channel/UCbetUssizXWLgZdDVEFp8Sg','isBaileys','{}.constructor(\x22return\x20this\x22)(\x20)','figlet','42EeGoWA','clui','\x20Booting\x20WhatsApp\x20Bot','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20','catch','yellow','parse','Good\x20Bye','toString','WhatsApp\x20Bot\x20Multi\x20Device','Standard','https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg','71055cyYEFj','\x20Selamat\x20Datang\x20Di\x20Grup\x20*','reply','add','2353440HXizUB','prefa','prototype','loggedOut','image','group-participants.update','connection.update','Deskripsi\x20:\x20','*\x0a\x0aIntro\x20Dulu\x20Yuk\x20Kak\x0a\x0a\x0a📛\x20Nama\x20:\x20\x0a🔞\x20Umur\x20:\x0a🏙️\x20Askot\x20:\x0a👫\x20Gender\x20:\x0a\x0aSemoga\x20Kamu\x20Senang\x20Berada\x20Disini\x20Serta\x20Jangan\x20Lupa\x20Untuk\x20Membaca\x20Dan\x20Mematuhi\x20Rules\x20Yang\x20Ada','(((.+)+)+)+$','bind','messages','length','@adiwajshing/baileys','return\x20(function()\x20','moment','log','\x0a\x0aTetap\x20Putus\x20Asa\x20Jangan\x20Semangat\x20Dan\x20Jadilah\x20Beban\x20Keluarga\x20🤙🗿','constructor','default','./message/msg','#welcome','7744080Gpmmas','error','groupMetadata','Youtube','./config.json','white','search','161183kejXTQ','console','messages.upsert','511oQVglI','Izumi-Bot','Rest\x20Api','1068SrJHrG','8mGsaXb','3EB0','creds.update','action','stop','resolve','+62\x20859-2116-5857','info','Izumi-Multi-Device','desc','subject','split','.json','#keluar','86320tuRNMc','trace','\x20sedang\x20diperhatikan\x20terhadap\x20perubahan','redBright','2080719UefdJB','participants','startsWith','10eohont','\x22\x20Telah\x20diupdate!','cyanBright','[\x20WHATSAPP\x20BOT\x20]\x20\x20','apply','textSync','pino','\x20:\x20','https://christian-id-api.herokuapp.com/docs','table','cyan','./message/help','format','greenBright','sendMessage','Wa\x20web\x20terlogout...','Follow\x20Insta\x20Christian','exception','Byeee\x20@','3.0','readFileSync','cache','statusCode','@chris.tianid','store','bold','profilePictureUrl','[\x20Powered\x20By\x20Christian\x20]','195512bZqUgl','key','child','./lib/myfunc','\x20Reconnecting\x20WhatsApp\x20Bot','sessionName'];_0x17ea=function(){return _0x13ff68;};return _0x17ea();}const _0x37d990=(function(){let _0x3b58a1=!![];return function(_0x1939cc,_0x28aa97){const _0x2de702=_0x3b58a1?function(){function _0x4d0617(_0x24f59c,_0x5b7010){return _0x17fd(_0x5b7010-0x49,_0x24f59c);}if(_0x28aa97){const _0x16399f=_0x28aa97[_0x4d0617(0x1e7,0x1c1)](_0x1939cc,arguments);return _0x28aa97=null,_0x16399f;}}:function(){};return _0x3b58a1=![],_0x2de702;};}()),_0x3f6254=_0x37d990(this,function(){const _0x4a45ef=function(){let _0x291956;try{_0x291956=Function(_0xf0390(-0x1b2,-0x1bf)+_0xf0390(-0x1cf,-0x1df)+');')();}catch(_0x20cb84){_0x291956=window;}function _0xf0390(_0x103352,_0x18787f){return _0x17fd(_0x18787f- -0x377,_0x103352);}return _0x291956;},_0x32799e=_0x4a45ef();function _0x4d17c2(_0x2a5fa7,_0x58afc5){return _0x17fd(_0x2a5fa7-0x45,_0x58afc5);}const _0x45f194=_0x32799e[_0x4d17c2(0x20d,0x240)]=_0x32799e[_0x4d17c2(0x20d,0x23e)]||{},_0x545cc7=[_0x4d17c2(0x1ff,0x1e0),'warn',_0x4d17c2(0x21a,0x212),_0x4d17c2(0x206,0x1f3),_0x4d17c2(0x1ca,0x1bf),_0x4d17c2(0x1c2,0x1e3),_0x4d17c2(0x222,0x21b)];for(let _0x3ed9ba=0x0;_0x3ed9ba<_0x545cc7[_0x4d17c2(0x1fb,0x224)];_0x3ed9ba++){const _0xf31aae=_0x37d990['constructor'][_0x4d17c2(0x1f1,0x1f7)][_0x4d17c2(0x1f9,0x1dc)](_0x37d990),_0x1f5c9f=_0x545cc7[_0x3ed9ba],_0x1a2c22=_0x45f194[_0x1f5c9f]||_0xf31aae;_0xf31aae['__proto__']=_0x37d990[_0x4d17c2(0x1f9,0x202)](_0x37d990),_0xf31aae[_0x4d17c2(0x1e7,0x207)]=_0x1a2c22['toString']['bind'](_0x1a2c22),_0x45f194[_0x1f5c9f]=_0xf31aae;}});_0x3f6254();'use strict';const {default:makeWASocket,BufferJSON,initInMemoryKeyStore,DisconnectReason,AnyMessageContent,makeInMemoryStore,useSingleFileAuthState,delay}=require(_0x465a50(-0x4d,-0x23)),figlet=require(_0x465a50(-0x3c,-0x41)),fs=require('fs');function _0x465a50(_0x132b66,_0x1c4eb){return _0x17fd(_0x1c4eb- -0x1da,_0x132b66);}const moment=require(_0x465a50(-0x12,-0x21)),chalk=require('chalk'),logg=require(_0x465a50(-0x88,-0x60)),clui=require(_0x465a50(-0x1c,-0x3f)),{Spinner}=clui,{serialize}=require('./lib/myfunc'),{color,mylog,infolog}=require('./lib/color'),time=moment(new Date())[_0x465a50(-0x31,-0x5a)]('HH:mm:ss\x20DD/MM/YYYY');let setting=JSON[_0x465a50(-0xd,-0x3a)](fs[_0x465a50(-0x22,-0x52)](_0x465a50(0x1f,-0x16))),session='./'+setting[_0x465a50(-0xd,-0x45)]+_0x465a50(0x13,0x0);const {state,saveState}=useSingleFileAuthState(session);function title(){console['clear']();function _0x5d52e2(_0x2be8b0,_0x18d546){return _0x465a50(_0x2be8b0,_0x18d546-0x5b0);}console[_0x5d52e2(0x5af,0x590)](chalk[_0x5d52e2(0x56b,0x563)]['red'](figlet[_0x5d52e2(0x57e,0x54f)](_0x5d52e2(0x59f,0x5a1),{'font':_0x5d52e2(0x5b0,0x57a),'horizontalLayout':_0x5d52e2(0x56c,0x593),'verticalLayout':'default','width':0x50,'whitespaceBreak':![]}))),console[_0x5d52e2(0x56c,0x590)](chalk['yellow'](_0x5d52e2(0x591,0x573)+chalk['green'](_0x5d52e2(0x55b,0x565))+'\x0a\x0a'+chalk[_0x5d52e2(0x560,0x575)](_0x5d52e2(0x569,0x5a1))+_0x5d52e2(0x53b,0x551)+chalk['white'](_0x5d52e2(0x59d,0x579))+'\x0a'+chalk[_0x5d52e2(0x545,0x575)](_0x5d52e2(0x571,0x55a))+_0x5d52e2(0x532,0x551)+chalk['white'](_0x5d52e2(0x52c,0x561))+'\x0a'+chalk[_0x5d52e2(0x560,0x575)]('Message\x20Me\x20On\x20WhatsApp')+_0x5d52e2(0x527,0x551)+chalk[_0x5d52e2(0x589,0x59b)](_0x5d52e2(0x58d,0x5aa))+'\x0a'+chalk[_0x5d52e2(0x562,0x575)](_0x5d52e2(0x5d2,0x5a2))+_0x5d52e2(0x582,0x551)+chalk[_0x5d52e2(0x5b7,0x59b)](_0x5d52e2(0x585,0x552))+'\x0a'+chalk['yellow'](_0x5d52e2(0x569,0x599))+'\x20:\x20'+chalk[_0x5d52e2(0x5a9,0x59b)](_0x5d52e2(0x54c,0x56c))+'\x0a'));}function nocache(_0x3c782c,_0x482a55=()=>{}){function _0x2a3b84(_0x541ff1,_0x20e6b4){return _0x465a50(_0x541ff1,_0x20e6b4-0x4d5);}console[_0x2a3b84(0x4a6,0x4b5)]('Module\x20'+_0x3c782c+_0x2a3b84(0x4e7,0x4d9)),fs['watchFile'](require['resolve'](_0x3c782c),async()=>{await uncache(require['resolve'](_0x3c782c)),_0x482a55(_0x3c782c);});}function uncache(_0x5a5a24='.'){return new Promise((_0x4aaa99,_0x55697e)=>{function _0x20c713(_0x4f890e,_0x569025){return _0x17fd(_0x4f890e-0x3b4,_0x569025);}try{delete require[_0x20c713(0x53d,0x55c)][require[_0x20c713(0x587,0x581)](_0x5a5a24)],_0x4aaa99();}catch(_0xb80cd7){_0x55697e(_0xb80cd7);}});}const status=new Spinner(chalk[_0x465a50(-0x4f,-0x5c)](_0x465a50(-0x5a,-0x3e))),starting=new Spinner(chalk[_0x465a50(-0x73,-0x5c)]('\x20Preparing\x20After\x20Connect')),reconnect=new Spinner(chalk[_0x465a50(0x22,0x5)](_0x465a50(-0x38,-0x46))),store=makeInMemoryStore({'logger':logg()[_0x465a50(-0x71,-0x48)]({'level':'fatal','stream':_0x465a50(-0x85,-0x4e)})}),connectToWhatsApp=async()=>{const _0x3a1424=makeWASocket({'printQRInTerminal':!![],'logger':logg({'level':'fatal'}),'auth':state,'browser':[_0x4df54f(-0x1fe,-0x1f0),'Safari',_0x4df54f(-0x24d,-0x27a)]});title(),store[_0x4df54f(-0x220,-0x1ff)](_0x3a1424['ev']),require(_0x4df54f(-0x255,-0x282)),require(_0x4df54f(-0x241,-0x230)),require(_0x4df54f(-0x216,-0x24c)),nocache(_0x4df54f(-0x255,-0x273),_0x515a9f=>console[_0x4df54f(-0x21a,-0x229)](chalk['greenBright'](_0x4df54f(-0x25d,-0x256))+time+chalk['cyanBright']('\x20\x22'+_0x515a9f+_0x4df54f(-0x1f0,-0x224)))),nocache(_0x4df54f(-0x241,-0x272),_0x1ce483=>console[_0x4df54f(-0x21a,-0x232)](chalk[_0x4df54f(-0x253,-0x227)]('[\x20WHATSAPP\x20BOT\x20]\x20\x20')+time+chalk[_0x4df54f(-0x25e,-0x264)]('\x20\x22'+_0x1ce483+_0x4df54f(-0x1f0,-0x1fa)))),nocache(_0x4df54f(-0x216,-0x221),_0x5405ae=>console[_0x4df54f(-0x21a,-0x219)](chalk['greenBright']('[\x20WHATSAPP\x20BOT\x20]\x20\x20')+time+chalk[_0x4df54f(-0x25e,-0x286)]('\x20\x22'+_0x5405ae+_0x4df54f(-0x1f0,-0x1e7)))),_0x3a1424['multi']=!![],_0x3a1424['nopref']=!![],_0x3a1424[_0x4df54f(-0x229,-0x24b)]='';function _0x4df54f(_0x428a7f,_0x43d43e){return _0x465a50(_0x43d43e,_0x428a7f- -0x1fa);}return _0x3a1424['ev']['on'](_0x4df54f(-0x20b,-0x1f1),async _0x54ccf1=>{if(!_0x54ccf1[_0x4a5cc8(0x21,0x57)])return;var _0x54c208=_0x54ccf1['messages'][0x0];_0x54c208=serialize(_0x3a1424,_0x54c208);function _0x4a5cc8(_0x2a9ea7,_0x262616){return _0x4df54f(_0x262616-0x276,_0x2a9ea7);}_0x54c208[_0x4a5cc8(0x30,0x39)]=_0x54c208['key']['id'][_0x4a5cc8(0x6e,0x84)]('BAE5')||_0x54c208[_0x4a5cc8(0x30,0x33)]['id']['startsWith'](_0x4a5cc8(0x5c,0x71)),require(_0x4a5cc8(0x2f,0x60))(_0x3a1424,_0x54c208,_0x54ccf1,setting,store);}),_0x3a1424['ev']['on'](_0x4df54f(-0x224,-0x231),_0x3d939a=>{const {connection:_0x47bee0,lastDisconnect:_0x4dae5c}=_0x3d939a;function _0x571e31(_0x34d31f,_0x2c28ee){return _0x4df54f(_0x2c28ee-0xa6,_0x34d31f);}_0x47bee0==='close'&&(status[_0x571e31(-0x152,-0x15c)](),reconnect[_0x571e31(-0x14c,-0x15c)](),starting[_0x571e31(-0x191,-0x15c)](),console['log'](mylog('Server\x20Ready\x20✓')),_0x4dae5c[_0x571e31(-0x14d,-0x16d)]?.['output']?.[_0x571e31(-0x1d9,-0x1a4)]!==DisconnectReason[_0x571e31(-0x199,-0x181)]?connectToWhatsApp():console[_0x571e31(-0x19b,-0x174)](mylog(_0x571e31(-0x1d6,-0x1ab))));}),_0x3a1424['ev']['on'](_0x4df54f(-0x204,-0x22b),()=>saveState),_0x3a1424['ev']['on'](_0x4df54f(-0x225,-0x221),async _0x3a7eb4=>{function _0x24b443(_0xbd4f3f,_0x4d8986){return _0x4df54f(_0x4d8986-0x4e,_0xbd4f3f);}try{let _0x11bc2c=await _0x3a1424[_0x24b443(-0x1a4,-0x1c4)](_0x3a7eb4['id']);for(let _0x11602a of _0x3a7eb4[_0x24b443(-0x18e,-0x1a5)]){try{var _0x57c907=await _0x3a1424[_0x24b443(-0x20d,-0x1f8)](_0x11602a,_0x24b443(-0x1bb,-0x1d8));}catch{var _0x57c907=_0x24b443(-0x1bc,-0x1e1);}if(_0x3a7eb4[_0x24b443(-0x1a9,-0x1b5)]==_0x24b443(-0x1c3,-0x1dd)){var _0x22e35d=[{'buttonId':_0x24b443(-0x1a9,-0x1c7),'buttonText':{'displayText':'Welcome'},'type':0x1}];_0x3a1424['sendMessage'](_0x3a7eb4['id'],{'caption':'Hallo\x20@'+_0x11602a[_0x24b443(-0x1bb,-0x1ad)]('@')[0x0]+_0x24b443(-0x215,-0x1df)+_0x11bc2c[_0x24b443(-0x17b,-0x1ae)]+_0x24b443(-0x1a8,-0x1d4),'image':{'url':_0x57c907},'buttons':_0x22e35d,'footer':_0x24b443(-0x1e6,-0x1d5)+_0x11bc2c[_0x24b443(-0x19c,-0x1af)],'mentions':[_0x11602a]});}else{if(_0x3a7eb4[_0x24b443(-0x180,-0x1b5)]=='remove'){var _0x22e35d=[{'buttonId':_0x24b443(-0x1bb,-0x1ab),'buttonText':{'displayText':_0x24b443(-0x1b0,-0x1e5)},'type':0x1}];_0x3a1424[_0x24b443(-0x233,-0x204)](_0x3a7eb4['id'],{'caption':_0x24b443(-0x206,-0x200)+_0x11602a[_0x24b443(-0x1be,-0x1ad)]('@')[0x0]+_0x24b443(-0x203,-0x1cb),'image':{'url':_0x57c907},'buttons':_0x22e35d,'footer':''+_0x11bc2c[_0x24b443(-0x1cb,-0x1ae)],'mentions':[_0x11602a]});}}}}catch(_0x59bfca){console['log'](_0x59bfca);}}),_0x3a1424[_0x4df54f(-0x22c,-0x247)]=(_0x2312a1,_0xd74d9a,_0x6872eb)=>_0x3a1424[_0x4df54f(-0x252,-0x22c)](_0x2312a1,{'text':_0xd74d9a},{'quoted':_0x6872eb}),_0x3a1424;};connectToWhatsApp()[_0x465a50(-0x19,-0x3c)](_0x4d03b6=>console[_0x465a50(-0x44,-0x20)](_0x4d03b6));
+Sc Ori By Arasya
+Sc Record By Christian ID
+Note : Jangan Perjualkan Sc Ini!!!
+
+Instagram Jojo : @arsrfi.jpg
+Youtube : Jojo Channel
+
+Instagram : @chris.tianid
+Youtube : Christian ID
+WhatsApp : 0859-2116-5857
+
+THANKS TO
+- Irfan Hardianto
+- Amell
+- Hardianto
+- Affis Junianto
+- Rafli Rusdiana
+- Febri
+- Jojo
+- Christian ID
+
+Terimakasih*/
+"use strict";
+const {
+	default: makeWASocket,
+	BufferJSON,
+	initInMemoryKeyStore,
+	DisconnectReason,
+	AnyMessageContent,
+        makeInMemoryStore,
+	useSingleFileAuthState,
+	delay
+} = require("@adiwajshing/baileys")
+const figlet = require("figlet");
+const fs = require("fs");
+const moment = require('moment')
+const chalk = require('chalk')
+const logg = require('pino')
+const clui = require('clui')
+const { Spinner } = clui
+const { serialize } = require("./lib/myfunc");
+const { color, mylog, infolog } = require("./lib/color");
+const time = moment(new Date()).format('HH:mm:ss DD/MM/YYYY')
+let setting = JSON.parse(fs.readFileSync('./config.json'));
+let session = `./${setting.sessionName}.json`
+const { state, saveState } = useSingleFileAuthState(session)
+
+function title() {
+      console.clear()
+	  console.log(chalk.bold.red(figlet.textSync('Izumi-Bot', {
+		font: 'Standard',
+		horizontalLayout: 'default',
+		verticalLayout: 'default',
+		width: 80,
+		whitespaceBreak: false
+	})))
+	console.log(chalk.yellow(`\n                        ${chalk.green('[ Powered By Christian ]')}\n\n${chalk.yellow('Izumi-Bot')} : ${chalk.white('WhatsApp Bot Multi Device')}\n${chalk.yellow('Follow Insta Christian')} : ${chalk.white('@chris.tianid')}\n${chalk.yellow('Message Me On WhatsApp')} : ${chalk.white('+62 859-2116-5857')}\n${chalk.yellow('Rest Api')} : ${chalk.white('https://christian-id-api.herokuapp.com/docs')}\n${chalk.yellow('Youtube')} : ${chalk.white('https://youtube.com/channel/UCbetUssizXWLgZdDVEFp8Sg')}\n`))
+}
+
+/**
+* Uncache if there is file change;
+* @param {string} module Module name or path;
+* @param {function} cb <optional> ;
+*/
+function nocache(module, cb = () => { }) {
+	console.log(`Module ${module} sedang diperhatikan terhadap perubahan`) 
+	fs.watchFile(require.resolve(module), async () => {
+		await uncache(require.resolve(module))
+		cb(module)
+	})
+}
+/**
+* Uncache a module
+* @param {string} module Module name or path;
+*/
+function uncache(module = '.') {
+	return new Promise((resolve, reject) => {
+		try {
+			delete require.cache[require.resolve(module)]
+			resolve()
+		} catch (e) {
+			reject(e)
+		}
+	})
+}
+
+const status = new Spinner(chalk.cyan(` Booting WhatsApp Bot`))
+const starting = new Spinner(chalk.cyan(` Preparing After Connect`))
+const reconnect = new Spinner(chalk.redBright(` Reconnecting WhatsApp Bot`))
+
+const store = makeInMemoryStore({ logger: logg().child({ level: 'fatal', stream: 'store' }) })
+
+const connectToWhatsApp = async () => {
+	const conn = makeWASocket({
+            printQRInTerminal: true,
+            logger: logg({ level: 'fatal' }),
+            auth: state,
+            browser: ["Izumi-Multi-Device", "Safari", "3.0"]
+        })
+	title()
+        store.bind(conn.ev)
+	
+	/* Auto Update */
+	require('./message/help')
+	require('./lib/myfunc')
+	require('./message/msg')
+	nocache('./message/help', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	nocache('./lib/myfunc', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	nocache('./message/msg', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	
+	conn.multi = true
+	conn.nopref = true
+	conn.prefa = ''
+	conn.ev.on('messages.upsert', async m => {
+		if (!m.messages) return;
+		var msg = m.messages[0]
+		msg = serialize(conn, msg)
+		msg.isBaileys = msg.key.id.startsWith('BAE5') || msg.key.id.startsWith('3EB0')
+		require('./message/msg')(conn, msg, m, setting, store)
+	})
+	conn.ev.on('connection.update', (update) => {
+		const { connection, lastDisconnect } = update
+		if (connection === 'close') {
+			status.stop()
+			reconnect.stop()
+			starting.stop()
+			console.log(mylog('Server Ready ✓'))
+			lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut 
+			? connectToWhatsApp()
+			: console.log(mylog('Wa web terlogout...'))
+		}
+	})
+	conn.ev.on('creds.update', () => saveState)
+	
+	conn.ev.on('group-participants.update', async (data) => {
+	try {
+	let metadata = await conn.groupMetadata(data.id)
+	  for (let i of data.participants) {
+		try {
+		  var pp_user = await conn.profilePictureUrl(i, 'image')
+		} catch {
+		  var pp_user = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+		}
+		if (data.action == "add") {
+		   var but = [{buttonId: `#welcome`, buttonText: { displayText: "Welcome" }, type: 1 }]
+		  conn.sendMessage(data.id, { caption: `Hallo @${i.split("@")[0]} Selamat Datang Di Grup *${metadata.subject}*\n\nIntro Dulu Yuk Kak\n\n\n📛 Nama : \n🔞 Umur :\n🏙️ Askot :\n👫 Gender :\n\nSemoga Kamu Senang Berada Disini Serta Jangan Lupa Untuk Membaca Dan Mematuhi Rules Yang Ada`, image: {url: pp_user}, buttons: but, footer: `Deskripsi : ${metadata.desc}`, mentions: [i]})
+		} else if (data.action == "remove") {
+		  var but = [{buttonId: `#keluar`, buttonText: { displayText: "Good Bye" }, type: 1 }]
+		  conn.sendMessage(data.id, { caption: `Byeee @${i.split("@")[0]}\n\nTetap Putus Asa Jangan Semangat Dan Jadilah Beban Keluarga 🤙🗿`, image: {url: pp_user}, buttons: but, footer: `${metadata.subject}`, mentions: [i]})
+		}
+	  }
+	} catch (e) {
+	  console.log(e)
+	}
+  }
+)
+
+	
+	conn.reply = (from, content, msg) => conn.sendMessage(from, { text: content }, { quoted: msg })
+
+	return conn
+}
+
+connectToWhatsApp()
+.catch(err => console.log(err))
